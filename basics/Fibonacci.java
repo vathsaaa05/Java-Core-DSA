17c623a5560b5fdd176ca074d886fbdb59c74352
Sreevathsa Tupakula <150000096+vathsaaa05@users.noreply.github.com>
2025-04-01 17:16:31 +0530

    Update Fibonacci.java

--- basics/Fibonacci.java
+++ basics/Fibonacci.java
@@ -7,17 +7,17 @@ import java.util.Scanner;
 public class Fibonacci {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int  num = sc.nextInt();
-        int a = 0;
-        int b = 1;
+        int num = sc.nextInt(); //Taking the nth number until the Fibonacci series should iterate.
+        int first = 0; //As the Fibonacci Series starts with 0.
+        int second = 1;
         int count = 2;
-        while(count <= num){
-            int temp = b;
-            b += a;
-            a = temp;
-            count ++;
+        //Loop will iterate until the count is greater than or equals to the input.
+        while (count <= num) {
+            int temp = second;
+            second += first;
+            first = temp;
+            count++;
         }
-        System.out.println(b);
-
+        System.out.println(second);
     }
 }
