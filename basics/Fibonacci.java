20e347d68ac03a1fff8c2dc6b378cb32eba2c7ca
Sreevathsa Tupakula <sreevathsatupakula@gmail.com>
2025-04-01 17:09:42 +0530

    Initial Fibonacci Commit

--- basics/Fibonacci.java
+++ basics/Fibonacci.java
@@ -1,4 +1,23 @@
+/* To calculate Fibonacci Series up to n numbers. */
+
 package basics;
 
+import java.util.Scanner;
+
 public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int  num = sc.nextInt();
+        int a = 0;
+        int b = 1;
+        int count = 2;
+        while(count <= num){
+            int temp = b;
+            b += a;
+            a = temp;
+            count ++;
+        }
+        System.out.println(b);
+
+    }
 }
